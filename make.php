f9194002c1d49bf0afda904a8c0f83da0957a858
Cvar1984 <gedzsarjuncomuniti@gmail.com>
2019-11-04 09:33:20 +0700

    compress phar data

--- make.php
+++ make.php
@@ -3,9 +3,11 @@
 $srcRoot = 'src';
 $buildRoot = 'build';
 
-$phar = new Phar($buildRoot . "/main.phar",FilesystemIterator::CURRENT_AS_FILEINFO | FilesystemIterator::KEY_AS_FILENAME, "main.phar");
-$phar["main.php"] = file_get_contents($srcRoot . "/main.php");
-$phar["website_parser.php"] = file_get_contents($srcRoot . "/website_parser.php");
+$phar = new Phar($buildRoot.'/main.phar',FilesystemIterator::CURRENT_AS_FILEINFO | FilesystemIterator::KEY_AS_FILENAME, 'main.phar');
+$phar['main.php']=file_get_contents($srcRoot.'/main.php');
+$phar['website_parser.php'] = file_get_contents($srcRoot . "/website_parser.php");
 $phar['sql.ini']=file_get_contents($srcRoot.'/sql.ini');
-$phar->setStub( "#!/usr/bin/env php\n".$phar->createDefaultStub("main.php"));
+$phar->compressFiles(Phar::BZ2);
+$phar->setSignatureAlgorithm(Phar::SHA1, sha1('Cvar1984'));
+$phar->setStub("#!/usr/bin/env php\n".$phar->createDefaultStub('main.php'));
 chmod($buildRoot.'/main.phar',0777);
