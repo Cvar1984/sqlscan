10c0907c70aa233fcb85650fab4a19d1cef13a39
Cvar1984 <gedzsarjuncomuniti@gmail.com>
2019-12-04 03:11:29 +0700

    add new modules

--- make.php
+++ make.php
@@ -2,17 +2,19 @@
 <?php
 $srcRoot = __DIR__ . DIRECTORY_SEPARATOR . 'src';
 $buildRoot = __DIR__ . DIRECTORY_SEPARATOR . 'build';
-
+$assetsRoot = __DIR__ . DIRECTORY_SEPARATOR . 'assets';
+$file = 'file_get_contents';
 $phar = new Phar(
     $buildRoot . DIRECTORY_SEPARATOR . 'main.phar',
     FilesystemIterator::CURRENT_AS_FILEINFO | FilesystemIterator::KEY_AS_FILENAME,
     'main.phar'
 );
-$phar['main.php'] = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'main.php');
-$phar['Sqlscan.php'] = file_get_contents($srcRoot . DIRECTORY_SEPARATOR . 'Sqlscan.php');
-$phar['WebsiteParser.php'] = file_get_contents($srcRoot . DIRECTORY_SEPARATOR . 'WebsiteParser.php');
-$phar['sql.ini'] = file_get_contents($srcRoot . DIRECTORY_SEPARATOR . 'sql.ini');
-$phar['banner.txt'] = file_get_contents($srcRoot . DIRECTORY_SEPARATOR . 'banner.txt');
+$phar['main.php'] = $file(__DIR__ . DIRECTORY_SEPARATOR . 'main.php');
+$phar['Sqlscan.php'] = $file($srcRoot . DIRECTORY_SEPARATOR . 'Sqlscan.php');
+$phar['WebsiteParser.php'] = $file($srcRoot . DIRECTORY_SEPARATOR . 'WebsiteParser.php');
+$phar['Cli.php'] = $file($srcRoot . DIRECTORY_SEPARATOR . 'Cli.php');
+$phar['sql.ini'] = $file($assetsRoot . DIRECTORY_SEPARATOR . 'sql.ini');
+$phar['banner.txt'] = $file($assetsRoot . DIRECTORY_SEPARATOR . 'banner.txt');
 $phar->compressFiles(Phar::BZ2);
 $phar->setSignatureAlgorithm(Phar::SHA1, sha1('Cvar1984'));
 $phar->setStub("#!/usr/bin/env php\n" . $phar->createDefaultStub('main.php'));
