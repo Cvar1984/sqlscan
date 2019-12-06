2f8e8de0dd0c9b9b157ab429dc8d68b8ef94f25b
Cvar1984 <gedzsarjuncomuniti@gmail.com>
2019-12-06 15:53:11 +0700

    fix cf psr2

--- main.php
+++ main.php
@@ -4,7 +4,7 @@ use Cvar1984\SqlScan\SqlScan;
 use Cvar1984\SqlScan\Cli;
 use Cvar1984\SqlScan\Dorker;
 try {
-    $file  = file_get_contents('phar://main.phar/assets/banner.txt');
+    $file = file_get_contents('phar://main.phar/assets/banner.txt');
     if ($file) {
         Cli::printStandar($file);
     } else {
@@ -12,39 +12,39 @@ try {
     }
     if ($argc >= 3) {
         switch ($argv[2]) {
-        case '--scan':
-            $sql = new Sqlscan();
-            $url = trim($argv[1]);
+            case '--scan':
+                $sql = new Sqlscan();
+                $url = trim($argv[1]);
 
-            if (filter_var($url, FILTER_VALIDATE_URL)) {
-                $sql->scan($url, 'result.txt');
-            } else {
-                $pwd = getcwd() . DIRECTORY_SEPARATOR . $url;
-                if (file_exists($pwd)) {
-                    $file = file_get_contents($pwd);
-                    $file = trim($file, "\n");
-                    $url  = explode("\n", $file);
-                    foreach ($url as $url) {
-                        $sql->scan($url, 'result.txt');
-                    }
+                if (filter_var($url, FILTER_VALIDATE_URL)) {
+                    $sql->scan($url, 'result.txt');
                 } else {
-                    Cli::printError('File not exists ' . $pwd);
+                    $pwd = getcwd() . DIRECTORY_SEPARATOR . $url;
+                    if (file_exists($pwd)) {
+                        $file = file_get_contents($pwd);
+                        $file = trim($file, "\n");
+                        $url  = explode("\n", $file);
+                        foreach ($url as $url) {
+                            $sql->scan($url, 'result.txt');
+                        }
+                    } else {
+                        Cli::printError('File not exists ' . $pwd);
+                    }
                 }
-            }
-            break;
+                break;
 
-        case '--dork':
-            $dork=new Dorker($argv[1], 'result_url.txt');
-            break;
+            case '--dork':
+                $dork = new Dorker($argv[1], 'result_url.txt');
+                break;
 
-        case '--shell':
-            Cli::printError('under development');
-            break;
+            case '--shell':
+                Cli::printError('under development');
+                break;
 
-        default:
-            Cli::printAsk('Available method : --scan, --dork, --shell');
-            Cli::printError('Undefined method : ' . $argv[2]);
-            break;
+            default:
+                Cli::printAsk('Available method : --scan, --dork, --shell');
+                Cli::printError('Undefined method : ' . $argv[2]);
+                break;
         }
     } else {
         Cli::printAsk('Usage : sqlscan [required] [option]');
--- src/Dorker.php
+++ src/Dorker.php
@@ -6,12 +6,13 @@ use Serps\SearchEngine\Google\GoogleUrl;
 use Serps\Core\Browser\Browser;
 use Serps\SearchEngine\Google\NaturalResultType;
 use Cvar1984\SqlScan\Cli;
+
 class Dorker
 {
     protected static $userAgent = "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.93 Safari/537.36";
     protected static $browserLanguage = "fr-FR";
-    protected static $error=null;
-    function __construct(string $dork, string $filename='result_url.txt')
+    protected static $error = null;
+    function __construct(string $dork, string $filename = 'result_url.txt')
     {
         $browser = new Browser(new CurlClient(), self::$userAgent, self::$browserLanguage);
         $googleClient = new GoogleClient($browser);
@@ -21,20 +22,19 @@ class Dorker
         $results = $response->getNaturalResults();
 
         foreach ($results as $result) {
-            if($result->is(NaturalResultType::CLASSICAL)){
+            if ($result->is(NaturalResultType::CLASSICAL)) {
                 Cli::printLine('title : ' . $result->title);
                 Cli::printLine('url : ' . $result->url);
-                $write=@fopen($filename, 'a');
-                if($write) {
+                $write = @fopen($filename, 'a');
+                if ($write) {
                     fprintf($write, '%s%s', $result->url, PHP_EOL);
                     fclose($write);
-                }
-                else {
-                    self::$error='Waring can\'t write result';
+                } else {
+                    self::$error = 'Waring can\'t write result';
                 }
             }
         }
-        if(!empty(self::$error)) {
+        if (!empty(self::$error)) {
             Cli::printWarning(self::$error);
         }
     }
