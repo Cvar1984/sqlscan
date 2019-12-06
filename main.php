7ebebe755e5477ef49640f9c3231617459654565
Cvar1984 <gedzsarjuncomuniti@gmail.com>
2019-12-06 15:49:43 +0700

    remove unused namwspace

--- main.php
+++ main.php
@@ -2,19 +2,13 @@
 require __DIR__ . '/vendor/autoload.php';
 use Cvar1984\SqlScan\SqlScan;
 use Cvar1984\SqlScan\Cli;
-use Serps\SearchEngine\Google\GoogleClient;
-use Serps\HttpClient\CurlClient;
-use Serps\SearchEngine\Google\GoogleUrl;
-use Serps\Core\Browser\Browser;
-use Serps\SearchEngine\Google\NaturalResultType;
-
+use Cvar1984\SqlScan\Dorker;
 try {
-    $print = new Cli();
     $file  = file_get_contents('phar://main.phar/assets/banner.txt');
     if ($file) {
-        $print->printStandar($file);
+        Cli::printStandar($file);
     } else {
-        $print->printWarning('can\'t load banner');
+        Cli::printWarning('can\'t load banner');
     }
     if ($argc >= 3) {
         switch ($argv[2]) {
@@ -34,42 +28,27 @@ try {
                         $sql->scan($url, 'result.txt');
                     }
                 } else {
-                    $print->printError('File not exists ' . $pwd);
+                    Cli::printError('File not exists ' . $pwd);
                 }
             }
             break;
 
         case '--dork':
-            $userAgent = "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.93 Safari/537.36";
-            $browserLanguage = "fr-FR";
-            $browser = new Browser(new CurlClient(), $userAgent, $browserLanguage);
-            $googleClient = new GoogleClient($browser);
-            $googleUrl = new GoogleUrl();
-            $googleUrl->setSearchTerm($argv[1]);
-            $response = $googleClient->query($googleUrl);
-            $results = $response->getNaturalResults();
-
-            foreach ($results as $result) {
-                if($result->is(NaturalResultType::CLASSICAL)){
-                    $print->printLine('title : ' . $result->title);
-                    $print->printLine('url : ' . $result->url);
-                }
-
-            }
+            $dork=new Dorker($argv[1], 'result_url.txt');
             break;
 
         case '--shell':
-            $print->printError('under development');
+            Cli::printError('under development');
             break;
 
         default:
-            $print->printAsk('Available method : --scan, --dork, --shell');
-            $print->printError('Undefined method : ' . $argv[2]);
+            Cli::printAsk('Available method : --scan, --dork, --shell');
+            Cli::printError('Undefined method : ' . $argv[2]);
             break;
         }
     } else {
-        $print->printAsk('Usage : sqlscan [required] [option]');
-        $print->printLine('Examples : sqlscan http://hackme.org --scan');
+        Cli::printAsk('Usage : sqlscan [required] [option]');
+        Cli::printLine('Examples : sqlscan http://hackme.org --scan');
     }
 } catch (Exception $e) {
     fprintf(STDERR, '%s%s', $e->getMessage(), PHP_EOL);
