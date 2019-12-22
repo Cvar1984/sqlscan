126ee2cb89413f24da175437679629da615b5cbe
Cvar1984 <gedzsarjuncomuniti@gmail.com>
2019-12-22 22:56:32 +0700

    Add progress bar

--- src/SqlScan.php
+++ src/SqlScan.php
@@ -32,34 +32,27 @@ class SqlScan  {
 
         if (!empty($count)) {
             foreach ($url as $urls) {
-                if (pathinfo($urls[0], PATHINFO_EXTENSION) == 'pdf') {
-                    continue;
-                } elseif (pathinfo($urls[0], PATHINFO_EXTENSION) == 'zip') {
-                    continue;
-                } elseif (pathinfo($urls[0], PATHINFO_EXTENSION) == 'mp4') {
-                    continue;
-                } elseif (pathinfo($urls[0], PATHINFO_EXTENSION) == 'mp3') {
-                    continue;
-                } elseif (pathinfo($urls[0], PATHINFO_EXTENSION) == 'tar') {
-                    continue;
-                } elseif (pathinfo($urls[0], PATHINFO_EXTENSION) == 'jpg') {
-                    continue;
-                } elseif (pathinfo($urls[0], PATHINFO_EXTENSION) == 'png') {
-                    continue;
-                } elseif (pathinfo($urls[0], PATHINFO_EXTENSION) == 'gif') {
-                    continue;
-                } elseif (pathinfo($urls[0], PATHINFO_EXTENSION) == 'm4a') {
-                    continue;
-                } elseif (pathinfo($urls[0], PATHINFO_EXTENSION) == '3gp') {
-                    continue;
-                }
-                if (!preg_match('/=/', $urls[0])) {
-                    continue;
-                }
-
-                $urls[0] = str_replace('=', '=\'', $urls[0]);
-                Cli::printLine('Testing : ' . $urls[0]);
-                $result = @file_get_contents($urls[0]);
+                if (pathinfo($urls[0], PATHINFO_EXTENSION) == 'pdf') continue;
+                elseif (pathinfo($urls[0], PATHINFO_EXTENSION) == 'zip') continue;
+                elseif (pathinfo($urls[0], PATHINFO_EXTENSION) == 'mp4') continue;
+                elseif (pathinfo($urls[0], PATHINFO_EXTENSION) == 'mp3') continue;
+                elseif (pathinfo($urls[0], PATHINFO_EXTENSION) == 'tar') continue;
+                elseif (pathinfo($urls[0], PATHINFO_EXTENSION) == 'jpg') continue;
+                elseif (pathinfo($urls[0], PATHINFO_EXTENSION) == 'png') continue;
+                elseif (pathinfo($urls[0], PATHINFO_EXTENSION) == 'gif') continue;
+                elseif (pathinfo($urls[0], PATHINFO_EXTENSION) == 'm4a') continue;
+                elseif (pathinfo($urls[0], PATHINFO_EXTENSION) == '3gp') continue;
+                if (!preg_match('/=/', $urls[0])) continue;
+                $urlz[] = $urls[0];
+            }
+            $count = count($urlz);
+            $progressBar = new \ProgressBar\Manager(0, $count);
+            Cli::printLine('Total available urls : ' . $count);
+            foreach ($urlz as $urls) {
+                $urls = str_replace('=', '=\'', $urls);
+                //Cli::printLine('Testing : ' . $urls);
+                $progressBar->advance();
+                $result = @file_get_contents($urls);
 
                 foreach (self::$sql as $sqli) {
                     if (preg_match('/' . $sqli . '/', $result)) {
@@ -68,7 +61,7 @@ class SqlScan  {
                         if (!$file) {
                             Cli::printWarning('warning can\'t write result');
                         } else {
-                            fprintf($file, $urls[0] . PHP_EOL);
+                            fprintf($file, $urls . PHP_EOL);
                             fclose($file);
                         }
                         break;
